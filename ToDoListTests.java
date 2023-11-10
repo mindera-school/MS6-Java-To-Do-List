7da95d4f68713f1d99deda9720c52bfc64722787
Fábio <fabio.goncalves@school.mindera.com>
2023-11-10 10:42:55 +0000

    2 unit tests were tested

--- ToDoListTests.java
+++ ToDoListTests.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ToDoListTests {
     // Redirect System.out to capture the printed output
@@ -29,9 +31,36 @@ public class ToDoListTests {
                 "\n\u001b[38;5;7m2. \u001b[38;5;40mTask 2 ✅\u001b[0m" +
                 "\n\u001b[38;5;7m3. \u001b[38;5;1mTask 3\u001b[0m" +
                 "\n\nThis To Do List has 3 tasks!\n" +
-                // Include expected output for task completion percentage and separators if needed
+                "\nThe task completion percentage is 33%!\n\n" +
                 "\u001b[38;5;8m------------------------\u001b[0m";
 
         assertEquals(expectedOutput.trim(), outContent.toString().trim());
+    }
+
+    @Test
+    public void testCreateTask() {
+        // Set up the test environment
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // Set up the input
+        String userInput = "New Task\n";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
+
+        // Create test data
+        String[] toDoList = new String[3];
+
+        // Call the method to be tested
+        ToDoList.createTask(toDoList);
+
+        // Restore the original System.in and System.out
+        System.setIn(System.in);
+        System.setOut(System.out);
+
+        // Verify the output and the updated toDoList
+        String expectedOutput = "\n\u001b[38;5;15mType in the task name: \u001b[0m" + "\n\u001b[38;5;10mThe task '\u001b[38;5;15mNew Task\u001b[38;5;10m' was created!\u001b[0m";
+        assertEquals(expectedOutput.trim(), outContent.toString().trim());
+        // Add more assertions based on the expected behavior of createTask
+        // For example, you might check if the task is added to the toDoList
     }
 }
